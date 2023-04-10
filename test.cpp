aa213e4e67bb036b846c13944b786752819c3162
MrStroopWafel <larsmulleneers@hotmail.nl>
2023-04-10 19:25:49 +0200

    file naam veranderd voor verduidelijking; referentie voor de injector: https://cocomelonc.github.io/tutorial/2022/04/20/malware-pers-1.html

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@ author: @cocomelonc
 #include <windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+  ShowWindow(GetConsoleWindow(), SW_HIDE);
   MessageBoxA(NULL, "Meow-meow!","=^..^=", MB_OK);
   return 0;
 }
