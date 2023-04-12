5a657127b4f58dcab80c59fd882f6575fb26c59c
Balonserrr <94622356+Balonserrr@users.noreply.github.com>
2023-04-12 02:09:38 +0200

    maakt hidden folder aan in Documents
    mogelijke optimalisatie:
    
    CoTaskMemFree verwijderen aangezien het programma kort is, maar er is een kans waardoor het systeem het geheugen niet automatisch vrijgeeft waardoor je dus een mega memory lek krijgt

---
+++ hiddenFolder.cpp
@@ -0,0 +1,35 @@
+#include <Shlobj.h>
+#include <string>
+
+// Handmatige fix voor FOLDERID_Documents
+#ifndef FOLDERID_Documents
+const GUID FOLDERID_Documents = {0xFDD39AD0, 0x238F, 0x46AF, {0xAD, 0xB4, 0x6C, 0x85, 0x48, 0x03, 0x69, 0xC7}};
+#endif
+
+int main()
+{
+    // pad naar de documentenmap van de pc ophalen
+    PWSTR documentsPath = nullptr;
+    SHGetKnownFolderPath(FOLDERID_Documents, KF_FLAG_DEFAULT, nullptr, &documentsPath);
+    std::wstring hiddenFolderPath(std::wstring(documentsPath) + L"\\test");
+
+    // Ole32.dll-library laden om CoTaskMemFree te kunnen gebruiken
+    HMODULE ole32Module = LoadLibrary(TEXT("Ole32.dll"));
+    typedef void (WINAPI *CoTaskMemFreeFuncPtr)(LPVOID);
+    CoTaskMemFreeFuncPtr CoTaskMemFreePtr = nullptr;
+
+    // adres van de CoTaskMemFree-functie uit de Ole32.dll-lib halen
+    if (ole32Module != NULL) {
+        CoTaskMemFreePtr = (CoTaskMemFreeFuncPtr)GetProcAddress(ole32Module, "CoTaskMemFree");
+    }
+
+    // CoTaskMemFree-functie gebruiken om het geheugen van documentsPath vrij te maken anders lekt er geheugen
+    if (CoTaskMemFreePtr != nullptr) {
+        CoTaskMemFreePtr(documentsPath);
+    }
+
+    // verborgen map genaamd "test" in de documentenmap van de pc aanmaken
+    CreateDirectoryW(hiddenFolderPath.c_str(), nullptr) && SetFileAttributesW(hiddenFolderPath.c_str(), FILE_ATTRIBUTE_HIDDEN);
+
+    return 0;
+}
